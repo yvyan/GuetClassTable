ba705164cd09fef0aa4c3ce2617e042fc4ccc262
Wing <843862803@qq.com>
2020-10-25 23:11:14 +0800

    fix:修复周次不能自加的bug

--- app/src/main/java/top/yvayn/guettable/data/GeneralData.java
+++ app/src/main/java/top/yvayn/guettable/data/GeneralData.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class GeneralData {
     private static GeneralData generalData;
     private static final String SHP_NAME = "GeneralData";
     private static final String WEEK = "week";
+    private static final String TIME = "time";
     SharedPreferences sharedPreferences;
 
     private int week;
+    private long time;
 
     private GeneralData(Activity activity) {
         sharedPreferences = activity.getApplication().getSharedPreferences(SHP_NAME, Context.MODE_PRIVATE);
@@ -25,22 +30,75 @@ public class GeneralData {
     }
 
     public int getWeek() {
-        return week;
+        int err = calcWeekOffset(new Date(time), new Date(System.currentTimeMillis()));
+        return week + err;
     }
 
     public void setWeek(int week) {
+        this.time = System.currentTimeMillis();
         this.week = week;
         save();
     }
 
     private void load() {
         week = sharedPreferences.getInt(WEEK, 1);
+        time = sharedPreferences.getLong(TIME, System.currentTimeMillis());
     }
 
     private void save() {
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong(TIME, time);
         editor.putInt(WEEK, week);
         editor.apply();
     }
+
+    public static int calcDayOffset(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        int day1 = cal1.get(Calendar.DAY_OF_YEAR);
+        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+        int year1 = cal1.get(Calendar.YEAR);
+        int year2 = cal2.get(Calendar.YEAR);
+        if (year1 != year2) {  //同一年
+            int timeDistance = 0;
+            for (int i = year1; i < year2; i++) {
+                if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {  //闰年
+                    timeDistance += 366;
+                } else {  //不是闰年
+
+                    timeDistance += 365;
+                }
+            }
+            return timeDistance + (day2 - day1);
+        } else { //不同年
+            return day2 - day1;
+        }
+    }
+
+
+    public static int calcWeekOffset(Date startTime, Date endTime) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(startTime);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        dayOfWeek = dayOfWeek - 1;
+        if (dayOfWeek == 0) dayOfWeek = 7;
+
+        int dayOffset = calcDayOffset(startTime, endTime);
+
+        int weekOffset = dayOffset / 7;
+        int a;
+        if (dayOffset > 0) {
+            a = (dayOffset % 7 + dayOfWeek > 7) ? 1 : 0;
+        } else {
+            a = (dayOfWeek + dayOffset % 7 < 1) ? -1 : 0;
+        }
+        weekOffset = weekOffset + a;
+        return weekOffset;
+    }
+
 
 }
