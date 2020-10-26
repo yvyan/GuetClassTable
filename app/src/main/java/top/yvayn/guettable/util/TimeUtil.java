e6f9f82020c399ad5dca1b560f2c3e7a75b459f5
Wing <843862803@qq.com>
2020-10-26 21:09:31 +0800

    perf:调整了计算时间间隔的方法的位置

--- app/src/main/java/top/yvayn/guettable/util/TimeUtil.java
+++ app/src/main/java/top/yvayn/guettable/util/TimeUtil.java
@@ -6,54 +6,51 @@ import java.util.Date;
 public class TimeUtil {
     private static final long DAY = (1000 * 60 * 60 * 24);
 
-    /**
-     * 获得两个日期间距多少天
-     *
-     * @param beginDate
-     * @param endDate
-     * @return
-     */
-    public static long getTimeDistance(Date beginDate, Date endDate) {
-        Calendar fromCalendar = Calendar.getInstance();
-        fromCalendar.setTime(beginDate);
-        fromCalendar.set(Calendar.HOUR_OF_DAY, fromCalendar.getMinimum(Calendar.HOUR_OF_DAY));
-        fromCalendar.set(Calendar.MINUTE, fromCalendar.getMinimum(Calendar.MINUTE));
-        fromCalendar.set(Calendar.SECOND, fromCalendar.getMinimum(Calendar.SECOND));
-        fromCalendar.set(Calendar.MILLISECOND, fromCalendar.getMinimum(Calendar.MILLISECOND));
+    public static int calcDayOffset(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
 
-        Calendar toCalendar = Calendar.getInstance();
-        toCalendar.setTime(endDate);
-        toCalendar.set(Calendar.HOUR_OF_DAY, fromCalendar.getMinimum(Calendar.HOUR_OF_DAY));
-        toCalendar.set(Calendar.MINUTE, fromCalendar.getMinimum(Calendar.MINUTE));
-        toCalendar.set(Calendar.SECOND, fromCalendar.getMinimum(Calendar.SECOND));
-        toCalendar.set(Calendar.MILLISECOND, fromCalendar.getMinimum(Calendar.MILLISECOND));
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        int day1 = cal1.get(Calendar.DAY_OF_YEAR);
+        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
 
-        long dayDistance = (toCalendar.getTime().getTime() - fromCalendar.getTime().getTime()) / DAY;
-        dayDistance = Math.abs(dayDistance);
+        int year1 = cal1.get(Calendar.YEAR);
+        int year2 = cal2.get(Calendar.YEAR);
+        if (year1 != year2) {  //同一年
+            int timeDistance = 0;
+            for (int i = year1; i < year2; i++) {
+                if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {  //闰年
+                    timeDistance += 366;
+                } else {  //不是闰年
 
-        return dayDistance;
+                    timeDistance += 365;
+                }
+            }
+            return timeDistance + (day2 - day1);
+        } else { //不同年
+            return day2 - day1;
+        }
     }
 
-    /**
-     * 获取两个日期的月数差
-     * @param fromDate
-     * @param toDate
-     * @return
-     */
-    public static long getDifferMonth(Date fromDate, Date toDate) {
-        Calendar fromDateCal = Calendar.getInstance();
-        Calendar toDateCal = Calendar.getInstance();
-        fromDateCal.setTime(fromDate);
-        toDateCal.setTime(toDate);
 
-        int fromYear =  fromDateCal.get(Calendar.YEAR);
-        int toYear = toDateCal.get((Calendar.YEAR));
-        if (fromYear == toYear) {
-            return Math.abs(fromDateCal.get(Calendar.MONTH) - toDateCal.get(Calendar.MONTH));
+    public static int calcWeekOffset(Date startTime, Date endTime) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(startTime);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        dayOfWeek = dayOfWeek - 1;
+        if (dayOfWeek == 0) dayOfWeek = 7;
+
+        int dayOffset = calcDayOffset(startTime, endTime);
+
+        int weekOffset = dayOffset / 7;
+        int a;
+        if (dayOffset > 0) {
+            a = (dayOffset % 7 + dayOfWeek > 7) ? 1 : 0;
         } else {
-            int fromMonth = 12 - (fromDateCal.get(Calendar.MONTH) + 1);
-            int toMonth = toDateCal.get(Calendar.MONTH) + 1;
-            return Math.abs(toYear - fromYear - 1) * 12 + fromMonth + toMonth;
+            a = (dayOfWeek + dayOffset % 7 < 1) ? -1 : 0;
         }
+        weekOffset = weekOffset + a;
+        return weekOffset;
     }
 }
