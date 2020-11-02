fd44a1a223770a6c43e9c300fe0431e8f1ee53b9
Wing <843862803@qq.com>
2020-11-02 17:11:04 +0800

    添加通用网络连接底层 正在编写中，未完成

---
+++ app/src/main/java/top/yvayn/guettable/Http/HttpConnectionAndCode.java
@@ -0,0 +1,79 @@
+package top.yvayn.guettable.Http;
+
+import java.net.HttpURLConnection;
+
+/**
+ * @clear
+ */
+public class HttpConnectionAndCode {
+    public HttpURLConnection c;
+    public int code;
+    public String comment;
+    public int resp_code;
+    public String cookie;
+    public Object obj;
+    public HttpConnectionAndCode(int code_){
+        c = null;
+        comment = null;
+        code = code_;
+        resp_code = 0;
+        cookie = null;
+        obj = null;
+    }
+    public HttpConnectionAndCode(int code_, int resp_code_){
+        c = null;
+        comment = null;
+        code = code_;
+        resp_code = resp_code_;
+        cookie = null;
+        obj = null;
+    }
+    public HttpConnectionAndCode(HttpURLConnection cn, int code_){
+        c = cn;
+        code = code_;
+        comment = null;
+        resp_code = 0;
+        cookie = null;
+        obj = null;
+    }
+    public HttpConnectionAndCode(HttpURLConnection cn, int code_, int resp_code_){
+        c = cn;
+        code = code_;
+        comment = null;
+        resp_code = resp_code_;
+        cookie = null;
+        obj = null;
+    }
+    public HttpConnectionAndCode(HttpURLConnection cn, int code_, String comm){
+        c = cn;
+        code = code_;
+        comment = comm;
+        resp_code = 0;
+        cookie = null;
+        obj = null;
+    }
+    public HttpConnectionAndCode(HttpURLConnection cn, int code_, String comm, int resp_code_){
+        c = cn;
+        code = code_;
+        comment = comm;
+        resp_code = resp_code_;
+        cookie = null;
+        obj = null;
+    }
+    public HttpConnectionAndCode(HttpURLConnection cn, int code_, String comm, String cookie_){
+        c = cn;
+        code = code_;
+        comment = comm;
+        resp_code = 0;
+        cookie = cookie_;
+        obj = null;
+    }
+    public HttpConnectionAndCode(HttpURLConnection cn, int code_, String comm, String cookie_, int resp_code_){
+        c = cn;
+        code = code_;
+        comment = comm;
+        resp_code = resp_code_;
+        cookie = cookie_;
+        obj = null;
+    }
+}
