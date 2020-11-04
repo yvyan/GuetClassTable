30e150ebd92d1df5a98dd0139796aede53f3b958
Wing <843862803@qq.com>
2020-11-04 23:03:15 +0800

    注意：该分支正在开发中，不可正常使用 feat:实现登陆功能（新版教务系统）

---
+++ app/src/main/java/top/yvyan/guettable/Gson/LoginResponse.java
@@ -0,0 +1,22 @@
+package top.yvyan.guettable.Gson;
+
+/**
+ * @clear
+ */
+public class LoginResponse {
+    private boolean success;
+    private String msg;
+    private String data;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
