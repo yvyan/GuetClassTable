4641c7a9a975d5658a69e5a68531e06a0a14d95a
tank <1764611430@qq.com>
2020-11-19 09:36:32 +0800

    修改了cardView布局 写好了adapter以及DetailActivity 存在问题：详细信息加载不出来

--- app/src/main/java/top/yvyan/guettable/DetailActivity.java
+++ app/src/main/java/top/yvyan/guettable/DetailActivity.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import top.yvyan.guettable.adapter.ClassDetailAdapter;
 import top.yvyan.guettable.data.DayClassData;
 
 public class DetailActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ClassDetailAdapter classDetailAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        DayClassData dayClassData = DayClassData.newInstance();
-        TextView detail = findViewById(R.id.detail);
-        detail.setText(dayClassData.getCourseBeans().toString());
-
+        recyclerView = findViewById(R.id.class_detail_recycleView);
+        classDetailAdapter = new ClassDetailAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(classDetailAdapter);
     }
 }
