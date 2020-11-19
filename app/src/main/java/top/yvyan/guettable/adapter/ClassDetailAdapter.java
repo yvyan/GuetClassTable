a0a0eaab06a03c7d8af7c580031ea0a6a255e3ea
tank <1764611430@qq.com>
2020-11-19 09:36:32 +0800

    修改了cardView布局 写好了adapter以及DetailActivity 存在问题：详细信息加载不出来

--- app/src/main/java/top/yvyan/guettable/adapter/ClassDetailAdapter.java
+++ app/src/main/java/top/yvyan/guettable/adapter/ClassDetailAdapter.java
@@ -13,13 +13,11 @@ import java.util.List;
 
 import top.yvyan.guettable.R;
 import top.yvyan.guettable.bean.CourseBean;
+import top.yvyan.guettable.data.DayClassData;
 
 public class ClassDetailAdapter extends RecyclerView.Adapter<ClassDetailAdapter.ClassDetailViewHolder> {
-    List<CourseBean> CourseInfo = new ArrayList<>();
+    DayClassData dayClassData = DayClassData.newInstance();
 
-    public void setCourseInfo(List<CourseBean> CourseInfo) {
-        this.CourseInfo = CourseInfo;
-    }
     @NonNull
     @Override
     public ClassDetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -30,7 +28,19 @@ public class ClassDetailAdapter extends RecyclerView.Adapter<ClassDetailAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull ClassDetailViewHolder holder, int position) {
-
+        if(dayClassData.getCourseBeans().get(position).isLab()) {
+            holder.tisLab.setText("实验");
+            holder.tname.setText(dayClassData.getCourseBeans().get(position).getName());
+            holder.tlibname.setText(dayClassData.getCourseBeans().get(position).getLibName());
+            holder.troom.setText(dayClassData.getCourseBeans().get(position).getRoom());
+            holder.tday.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getDay()));
+            holder.ttime.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getTime()));
+            holder.tweekStart.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getWeekStart()));
+            holder.tweekEnd.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getWeekEnd()));
+        }
+        else {
+            holder.tisLab.setText("理论");
+        }
     }
 
     @Override
