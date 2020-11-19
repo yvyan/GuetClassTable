20da85549e7bb29bbaa4a3a9b9f352d09c64f442
tank <1764611430@qq.com>
2020-11-19 11:46:32 +0800

    实验课所有详细信息显示完成 理论课还没完成

--- app/src/main/java/top/yvyan/guettable/adapter/ClassDetailAdapter.java
+++ app/src/main/java/top/yvyan/guettable/adapter/ClassDetailAdapter.java
@@ -8,11 +8,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import top.yvyan.guettable.R;
-import top.yvyan.guettable.bean.CourseBean;
 import top.yvyan.guettable.data.DayClassData;
 
 public class ClassDetailAdapter extends RecyclerView.Adapter<ClassDetailAdapter.ClassDetailViewHolder> {
@@ -29,38 +25,66 @@ public class ClassDetailAdapter extends RecyclerView.Adapter<ClassDetailAdapter.
     @Override
     public void onBindViewHolder(@NonNull ClassDetailViewHolder holder, int position) {
         if(dayClassData.getCourseBeans().get(position).isLab()) {
-            holder.tisLab.setText("实验");
-            holder.tname.setText(dayClassData.getCourseBeans().get(position).getName());
-            holder.tlibname.setText(dayClassData.getCourseBeans().get(position).getLibName());
-            holder.troom.setText(dayClassData.getCourseBeans().get(position).getRoom());
-            holder.tday.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getDay()));
-            holder.ttime.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getTime()));
-            holder.tweekStart.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getWeekStart()));
-            holder.tweekEnd.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getWeekEnd()));
+            holder.tName.setText(dayClassData.getCourseBeans().get(position).getName());
+            holder.tTeacherName.setText(dayClassData.getCourseBeans().get(position).getTeacher());
+            holder.tLibname.setText(dayClassData.getCourseBeans().get(position).getLibName());
+            holder.tRoom.setText(dayClassData.getCourseBeans().get(position).getRoom());
+            holder.tDay.setText(whichDay(dayClassData.getCourseBeans().get(position).getDay()));
+            holder.tTime.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getTime()));
+            holder.tWeekStart.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getWeekStart()));
+            holder.tWeekEnd.setText(String.valueOf(dayClassData.getCourseBeans().get(position).getWeekEnd()));
+            holder.tRemarks.setText(dayClassData.getCourseBeans().get(position).getRemarks());
         }
         else {
-            holder.tisLab.setText("理论");
         }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dayClassData.getCourseBeans().size();
+    }
+
+    public String whichDay(int number){
+        String s = new String();
+        switch (number){
+            case 1:
+                s = "星期一";
+                break;
+            case 2:
+                s = "星期二";
+                break;
+            case 3:
+                s = "星期三";
+                break;
+            case 4:
+                s = "星期四";
+                break;
+            case 5:
+                s = "星期五";
+                break;
+            case 6:
+                s = "星期六";
+                break;
+            case 7:
+                s = "星期日";
+                break;
+        }
+        return s;
     }
 
     static class ClassDetailViewHolder extends RecyclerView.ViewHolder {
-        TextView tisLab, tname, tlibname, troom, tday, ttime, tweekStart, tweekEnd;
+        TextView tName, tTeacherName, tLibname, tRoom, tDay, tTime, tWeekStart, tWeekEnd, tRemarks;
         public ClassDetailViewHolder(@NonNull View itemView) {
             super(itemView);
-            tisLab = itemView.findViewById(R.id.isLab);
-            tname = itemView.findViewById(R.id.name);
-            tlibname = itemView.findViewById(R.id.libname);
-            troom = itemView.findViewById(R.id.room);
-            tday = itemView.findViewById(R.id.day);
-            ttime = itemView.findViewById(R.id.time);
-            tweekStart = itemView.findViewById(R.id.weekStart);
-            tweekEnd = itemView.findViewById(R.id.weekEnd);
-
+            tName = itemView.findViewById(R.id.textViewName);
+            tTeacherName = itemView.findViewById(R.id.textViewTeacherName);
+            tLibname = itemView.findViewById(R.id.textViewLibname);
+            tRoom = itemView.findViewById(R.id.textViewRoom);
+            tDay = itemView.findViewById(R.id.textViewDay);
+            tTime = itemView.findViewById(R.id.textViewTime);
+            tWeekStart = itemView.findViewById(R.id.textViewWeekStart);
+            tWeekEnd = itemView.findViewById(R.id.textViewWeekEnd);
+            tRemarks = itemView.findViewById(R.id.textViewRemarks);
         }
     }
 }
