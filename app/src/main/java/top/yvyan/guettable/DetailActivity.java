29779773434843f42fd408b831deeb476b3a71f3
Wing <843862803@qq.com>
2020-11-19 22:07:05 +0800

    refactor:ClassDetailAdapter与DayClassData解耦

--- app/src/main/java/top/yvyan/guettable/DetailActivity.java
+++ app/src/main/java/top/yvyan/guettable/DetailActivity.java
@@ -6,7 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Collections;
+import java.util.List;
+
 import top.yvyan.guettable.adapter.ClassDetailAdapter;
+import top.yvyan.guettable.bean.CourseBean;
+import top.yvyan.guettable.data.DayClassData;
+import top.yvyan.guettable.util.ComparatorCourse;
 
 public class DetailActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -16,8 +22,11 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        List<CourseBean> courseBeans = DayClassData.newInstance().getCourseBeans();
+        ComparatorCourse comparatorCourse = new ComparatorCourse();
+        Collections.sort(courseBeans, comparatorCourse);
         recyclerView = findViewById(R.id.class_detail_recycleView);
-        classDetailAdapter = new ClassDetailAdapter();
+        classDetailAdapter = new ClassDetailAdapter(courseBeans);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(classDetailAdapter);
     }
