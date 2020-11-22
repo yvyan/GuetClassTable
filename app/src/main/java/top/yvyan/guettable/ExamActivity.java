5166c6d2db1c3837498e8b80c1a90fc62b21bdce
Wing <843862803@qq.com>
2020-11-22 23:39:57 +0800

    feat:获取实验（不包含视图）

--- app/src/main/java/top/yvyan/guettable/ExamActivity.java
+++ app/src/main/java/top/yvyan/guettable/ExamActivity.java
@@ -1,16 +1,26 @@
 package top.yvyan.guettable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.zhuangfei.timetable.model.Schedule;
+import com.zhuangfei.timetable.model.ScheduleSupport;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import top.yvyan.guettable.adapter.ClassDetailAdapter;
+import top.yvyan.guettable.bean.CourseBean;
 import top.yvyan.guettable.bean.ExamBean;
 import top.yvyan.guettable.data.AccountData;
 import top.yvyan.guettable.data.GeneralData;
 import top.yvyan.guettable.data.MoreDate;
 import top.yvyan.guettable.service.GetDataService;
+import top.yvyan.guettable.util.TimeUtil;
 import top.yvyan.guettable.util.ToastUtil;
 
 public class ExamActivity extends AppCompatActivity {
@@ -18,6 +28,9 @@ public class ExamActivity extends AppCompatActivity {
     private AccountData accountData;
     private GeneralData generalData;
     private MoreDate moreDate;
+
+    private TextView examState;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,11 @@ public class ExamActivity extends AppCompatActivity {
         accountData = AccountData.newInstance(this);
         generalData = GeneralData.newInstance(this);
         moreDate = MoreDate.newInstance(this);
+
+        examState = findViewById(R.id.exam_state);
+        textView = findViewById(R.id.testShow);
+
+        updateView();
 
         new Thread(() -> {
             List<ExamBean> examBeans;
@@ -41,11 +59,24 @@ public class ExamActivity extends AppCompatActivity {
                 if (examBeans != null) {
                     moreDate.setExamBeans(examBeans);
                     runOnUiThread(() -> {
-                        ToastUtil.showToast(this, examBeans.toString());
+                        examState.setText("考试安排 更新成功");
+                        updateView();
                     });
                 }
+            } else {
+                runOnUiThread(() -> {
+                    examState.setText("考试安排 网络错误，从本地导入");
+                    updateView();
+                });
             }
         }).start();
+    }
 
+    /**
+     * 更新考试安排视图
+     */
+    public void updateView() {
+        List<ExamBean> examBeans = moreDate.getExamBeans();
+        textView.setText(examBeans.toString());
     }
 }
