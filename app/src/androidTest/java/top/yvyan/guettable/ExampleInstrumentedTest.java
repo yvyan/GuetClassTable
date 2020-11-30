bf94d2ad2ea8ff8e1b625674b2500f837f942f17
Wing <843862803@qq.com>
2020-11-30 15:00:33 +0800

    fix:包名错误的bug

--- app/src/androidTest/java/top/yvyan/guettable/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/top/yvyan/guettable/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("top.yvayn.guettable", appContext.getPackageName());
+        assertEquals("top.yvyan.guettable", appContext.getPackageName());
     }
 }
