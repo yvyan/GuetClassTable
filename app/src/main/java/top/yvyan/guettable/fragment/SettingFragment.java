47dadc00f15b1bdcdb95199c8d0e17e76eaaaec7
uuin <1429546779@qq.com>
2020-12-05 20:54:25 +0800

    设置 12/5

--- app/src/main/java/top/yvyan/guettable/fragment/SettingFragment.java
+++ app/src/main/java/top/yvyan/guettable/fragment/SettingFragment.java
@@ -12,37 +12,45 @@ import android.preference.SwitchPreference;
 import android.widget.Toast;
 
 import top.yvyan.guettable.R;
+import top.yvyan.guettable.util.ToastUtil;
 
 public class SettingFragment extends PreferenceFragment {
     private PreferenceCategory basic_settings;
     private SwitchPreference  refresh_data;
-    private ListPreference list_preference;
+    private ListPreference list_day;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_setting);
-
         basic_settings = (PreferenceCategory) findPreference("basic_settings");
         refresh_data = (SwitchPreference) findPreference("refresh_data");
-        list_preference = (ListPreference) findPreference("list_preference");
+        list_day = (ListPreference) findPreference("list_day");
 
 
         refresh_data.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 if((boolean)newValue) {
-                    basic_settings.addPreference(list_preference);
-                    Toast.makeText(getContext(), "已开启", Toast.LENGTH_LONG).show();
-
+                    basic_settings.addPreference(list_day);
+                    ToastUtil.showLongToast(getContext(),"已开启");
                 } else {
-                    basic_settings.removePreference(list_preference);
-                    Toast.makeText(getContext(), "已关闭", Toast.LENGTH_LONG).show();
+                    basic_settings.removePreference(list_day);
+                    ToastUtil.showLongToast(getContext(),"已关闭");
                 }
                 return true;
             }
         });
+
+        list_day.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                list_day.setSummary("刷新频率："+newValue.toString());
+                return true;
+            }
+        });
+
     }
 
 
@@ -50,6 +58,7 @@ public class SettingFragment extends PreferenceFragment {
     public void getSp(){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
         boolean switchRealTime = sp.getBoolean("refresh_data",false);
+
     }
 
     //配置全局监听，当改变的时候触发
