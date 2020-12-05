1f4be387712513a02a6e5a9c1ab7c4d374dfe332
uuin <1429546779@qq.com>
2020-12-05 20:54:25 +0800

    设置 12/5

--- app/src/main/java/top/yvyan/guettable/SettingActivity.java
+++ app/src/main/java/top/yvyan/guettable/SettingActivity.java
@@ -4,6 +4,8 @@ package top.yvyan.guettable;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import top.yvyan.guettable.fragment.SettingFragment;
 
@@ -14,10 +16,18 @@ public class SettingActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.setting);
+        ImageView back= (ImageView) findViewById(R.id.setting_back);
+        back.setOnClickListener(view -> {
+            finish();
+        });
+
         SettingFragment settingFragment = new SettingFragment();
         getFragmentManager().beginTransaction()
                 .add(android.R.id.content, settingFragment)
                 .commit();
+
     }
 
 }
