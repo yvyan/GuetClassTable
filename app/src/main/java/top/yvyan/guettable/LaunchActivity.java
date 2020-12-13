9e3f6d296e4eebcf160f559f3f0461faea728870
Wing <843862803@qq.com>
2020-12-13 13:46:26 +0800

    feat:启动页面，减少视觉上APP启动等待时间

---
+++ app/src/main/java/top/yvyan/guettable/LaunchActivity.java
@@ -0,0 +1,38 @@
+package top.yvyan.guettable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        Window window = this.getWindow();
+        if (Build.VERSION.SDK_INT >= 19) {
+            //透明状态栏
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
+        Integer time = 20;    //设置等待时间，单位为毫秒
+        Handler handler = new Handler();
+        //当计时结束时，跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                LaunchActivity.this.finish();
+            }
+        }, time);
+    }
+}
