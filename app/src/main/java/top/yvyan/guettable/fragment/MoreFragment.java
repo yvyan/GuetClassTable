aa26ad7fa9f101a6f9dd12210582270a1016cf96
Wing <843862803@qq.com>
2020-12-18 18:28:24 +0800

    perf:界面UI优化

--- app/src/main/java/top/yvyan/guettable/fragment/MoreFragment.java
+++ app/src/main/java/top/yvyan/guettable/fragment/MoreFragment.java
@@ -22,7 +22,11 @@ public class MoreFragment extends Fragment implements View.OnClickListener {
     private static MoreFragment moreFragment;
 
     private View view;
-    private View more_item_1, more_item_2, more_item_3, more_item_4, more_item_5, more_item_6, more_index;
+    private View testSchedule, credits, testScores, libScores;
+    private View graduationRequirement, planCourses, cet;
+    private View urlBkjw, urlVPN, urlCampus, urlStaff, urlMore;
+    private View evaluatingTeachers, evaluatingTextbooks;
+
 
     public MoreFragment() {
         // Required empty public constructor
@@ -40,20 +44,37 @@ public class MoreFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragement_more, container, false);
 
-        more_item_1 = view.findViewById(R.id.more_item_1);
-        more_item_2 = view.findViewById(R.id.more_item_2);
-        more_item_3 = view.findViewById(R.id.more_item_3);
-        more_item_4 = view.findViewById(R.id.more_item_4);
-        more_item_5 = view.findViewById(R.id.more_item_5);
-        more_item_6 = view.findViewById(R.id.more_item_6);
-        more_index = view.findViewById(R.id.more_index);
-        more_item_1.setOnClickListener(this);
-        more_item_2.setOnClickListener(this);
-        more_item_3.setOnClickListener(this);
-        more_item_4.setOnClickListener(this);
-        more_item_5.setOnClickListener(this);
-        more_item_6.setOnClickListener(this);
-        more_index.setOnClickListener(this);
+        testSchedule = view.findViewById(R.id.more_test_schedule);
+        testSchedule.setOnClickListener(this);
+        credits = view.findViewById(R.id.more_credits);
+        credits.setOnClickListener(this);
+        testScores = view.findViewById(R.id.more_test_scores);
+        testScores.setOnClickListener(this);
+        libScores = view.findViewById(R.id.more_lib_scores);
+        libScores.setOnClickListener(this);
+
+        graduationRequirement = view.findViewById(R.id.more_graduation_requirement);
+        graduationRequirement.setOnClickListener(this);
+        planCourses = view.findViewById(R.id.more_plan_courses);
+        planCourses.setOnClickListener(this);
+        cet = view.findViewById(R.id.more_cet);
+        cet.setOnClickListener(this);
+
+        urlBkjw = view.findViewById(R.id.more_url_bkjw);
+        urlBkjw.setOnClickListener(this);
+        urlVPN = view.findViewById(R.id.more_url_vpn);
+        urlVPN.setOnClickListener(this);
+        urlCampus = view.findViewById(R.id.more_url_campus);
+        urlCampus.setOnClickListener(this);
+        urlStaff = view.findViewById(R.id.more_url_staff);
+        urlStaff.setOnClickListener(this);
+        urlMore = view.findViewById(R.id.more_url_more);
+        urlMore.setOnClickListener(this);
+
+        evaluatingTeachers = view.findViewById(R.id.more_evaluating_teachers);
+        evaluatingTeachers.setOnClickListener(this);
+        evaluatingTextbooks = view.findViewById(R.id.more_evaluating_textbooks);
+        evaluatingTextbooks.setOnClickListener(this);
 
         return view;
     }
@@ -62,36 +83,37 @@ public class MoreFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         Intent intent;
         switch (view.getId()) {
-            case R.id.more_item_1:
+            case R.id.more_test_schedule:
                 intent = new Intent(getContext(), ExamActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.more_item_2:
-                intent = new Intent(getContext(), CETActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.more_item_3:
+            case R.id.more_test_scores:
                 intent = new Intent(getContext(), ExamScoreActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.more_item_4:
+            case R.id.more_lib_scores:
                 intent = new Intent(getContext(), ExperimentScoreActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.more_item_5:
-                intent = new Intent(getContext(), AverageTeacherActivity.class);
+
+            case R.id.more_cet:
+                intent = new Intent(getContext(), CETActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.more_item_6:
-                ToastUtil.showToast(getContext(), "下版本更新，敬请期待！");
-                break;
-            case R.id.more_index:
+
+            case R.id.more_url_more:
                 Uri uri = Uri.parse(getContext().getResources().getString(R.string.guet_yvyan_top));
                 intent = new Intent();
                 intent.setAction("android.intent.action.VIEW");
                 intent.setData(uri);
                 startActivity(intent);
                 break;
+
+            case R.id.more_evaluating_teachers:
+                intent = new Intent(getContext(), AverageTeacherActivity.class);
+                startActivity(intent);
+                break;
+
             default:
                 ToastUtil.showToast(getContext(), "敬请期待！");
         }
