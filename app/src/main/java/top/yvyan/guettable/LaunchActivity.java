66c959e960d986dd681c9f5a6efc0909314fafd7
Wing <843862803@qq.com>
2020-12-18 22:27:54 +0800

    perf:个人信息页UI

--- app/src/main/java/top/yvyan/guettable/LaunchActivity.java
+++ app/src/main/java/top/yvyan/guettable/LaunchActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -15,6 +16,14 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.app_white));              //设置状态栏颜色
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR); //状态栏为白色 图标显示深色
+        }
+
 
         Window window = this.getWindow();
         if (Build.VERSION.SDK_INT >= 19) {
