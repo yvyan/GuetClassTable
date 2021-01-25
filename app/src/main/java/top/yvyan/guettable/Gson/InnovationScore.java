8795bed7f516bbed99414e2067cbef1d034195d4
SSbird <594428581@qq.com>
2021-01-25 14:21:22 +0800

    加入创新积分查询功能

---
+++ app/src/main/java/top/yvyan/guettable/Gson/InnovationScore.java
@@ -0,0 +1,148 @@
+package top.yvyan.guettable.Gson;
+
+public class InnovationScore {
+    private String stid;
+    private String grade;
+    private String spno;
+    private String name;
+    private String spname;
+    private String dptno;
+    private String dptname;
+    private String classno;
+    private String msg;
+    private double pass;
+    private double lack;
+    private double lb1;
+    private double lb21;
+    private double lb22;
+    private double lb3;
+    private String changetype;
+
+    public String getStid() {
+        return stid;
+    }
+
+    public void setStid(String stid) {
+        this.stid = stid;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getSpno() {
+        return spno;
+    }
+
+    public void setSpno(String spno) {
+        this.spno = spno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpname() {
+        return spname;
+    }
+
+    public void setSpname(String spname) {
+        this.spname = spname;
+    }
+
+    public String getDptno() {
+        return dptno;
+    }
+
+    public void setDptno(String dptno) {
+        this.dptno = dptno;
+    }
+
+    public String getDptname() {
+        return dptname;
+    }
+
+    public void setDptname(String dptname) {
+        this.dptname = dptname;
+    }
+
+    public String getClassno() {
+        return classno;
+    }
+
+    public void setClassno(String classno) {
+        this.classno = classno;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public double getPass() {
+        return pass;
+    }
+
+    public void setPass(double pass) {
+        this.pass = pass;
+    }
+
+    public double getLack() {
+        return lack;
+    }
+
+    public void setLack(double lack) {
+        this.lack = lack;
+    }
+
+    public double getLb1() {
+        return lb1;
+    }
+
+    public void setLb1(double lb1) {
+        this.lb1 = lb1;
+    }
+
+    public double getLb21() {
+        return lb21;
+    }
+
+    public void setLb21(double lb21) {
+        this.lb21 = lb21;
+    }
+
+    public double getLb22() {
+        return lb22;
+    }
+
+    public void setLb22(double lb22) {
+        this.lb22 = lb22;
+    }
+
+    public double getLb3() {
+        return lb3;
+    }
+
+    public void setLb3(double lb3) {
+        this.lb3 = lb3;
+    }
+
+    public String getChangetype() {
+        return changetype;
+    }
+
+    public void setChangetype(String changetype) {
+        this.changetype = changetype;
+    }
+}
