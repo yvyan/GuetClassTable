222b4b3fb6e695862f2276d29713a82837eb260d
SSbird <594428581@qq.com>
2021-03-05 20:23:24 +0800

    perf:删除Json实体类无用属性

--- app/src/main/java/top/yvyan/guettable/Gson/ExamScore.java
+++ app/src/main/java/top/yvyan/guettable/Gson/ExamScore.java
@@ -3,22 +3,11 @@ package top.yvyan.guettable.Gson;
 import top.yvyan.guettable.bean.ExamScoreBean;
 
 public class ExamScore {
-    private String dptno;
-    private String dptname;
-    private String spno;
-    private String spname;
-    private String bj;
-    private int grade;
-    private String stid;
     private String name;
     private String term;
-    private String courseid;
-    private String courseno;
     private String cname;
-    private String courselevel;
     private float score;
     private String zpxs;
-    private String kctype;
     private String typeno;
     private String cid;
     private String cno;
@@ -27,75 +16,16 @@ public class ExamScore {
     private float pscj;
     private float khcj;
     private float zpcj;
-    private String kslb;
-    private String cjlb;
-    private int kssj;
     private float xf;
-    private String xslb;
-    private String tname1;
-    private int stage;
-    private String examt;
-    private int xs;
-    private int cjlx;
-    private int chk;
-    private String comm;
 
     public ExamScoreBean toExamScoreBean() {
         return new ExamScoreBean(cid, cname, cno, term, zpxs, score, pscj, sycj, khcj, xf, typeno);
     }
 
-    public void setDptno(String dptno) {
-        this.dptno = dptno;
-    }
-    public String getDptno() {
-        return dptno;
-    }
-
-    public void setDptname(String dptname) {
-        this.dptname = dptname;
-    }
-    public String getDptname() {
-        return dptname;
-    }
-
-    public void setSpno(String spno) {
-        this.spno = spno;
-    }
-    public String getSpno() {
-        return spno;
-    }
-
-    public void setSpname(String spname) {
-        this.spname = spname;
-    }
-    public String getSpname() {
-        return spname;
-    }
-
-    public void setBj(String bj) {
-        this.bj = bj;
-    }
-    public String getBj() {
-        return bj;
-    }
-
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
-    public int getGrade() {
-        return grade;
-    }
-
-    public void setStid(String stid) {
-        this.stid = stid;
-    }
-    public String getStid() {
-        return stid;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
+
     public String getName() {
         return name;
     }
@@ -103,41 +33,23 @@ public class ExamScore {
     public void setTerm(String term) {
         this.term = term;
     }
+
     public String getTerm() {
         return term;
-    }
-
-    public void setCourseid(String courseid) {
-        this.courseid = courseid;
-    }
-    public String getCourseid() {
-        return courseid;
-    }
-
-    public void setCourseno(String courseno) {
-        this.courseno = courseno;
-    }
-    public String getCourseno() {
-        return courseno;
     }
 
     public void setCname(String cname) {
         this.cname = cname;
     }
+
     public String getCname() {
         return cname;
-    }
-
-    public void setCourselevel(String courselevel) {
-        this.courselevel = courselevel;
-    }
-    public String getCourselevel() {
-        return courselevel;
     }
 
     public void setScore(float score) {
         this.score = score;
     }
+
     public float getScore() {
         return score;
     }
@@ -145,20 +57,15 @@ public class ExamScore {
     public void setZpxs(String zpxs) {
         this.zpxs = zpxs;
     }
+
     public String getZpxs() {
         return zpxs;
-    }
-
-    public void setKctype(String kctype) {
-        this.kctype = kctype;
-    }
-    public String getKctype() {
-        return kctype;
     }
 
     public void setTypeno(String typeno) {
         this.typeno = typeno;
     }
+
     public String getTypeno() {
         return typeno;
     }
@@ -166,6 +73,7 @@ public class ExamScore {
     public void setCid(String cid) {
         this.cid = cid;
     }
+
     public String getCid() {
         return cid;
     }
@@ -173,6 +81,7 @@ public class ExamScore {
     public void setCno(String cno) {
         this.cno = cno;
     }
+
     public String getCno() {
         return cno;
     }
@@ -180,6 +89,7 @@ public class ExamScore {
     public void setSycj(float sycj) {
         this.sycj = sycj;
     }
+
     public float getSycj() {
         return sycj;
     }
@@ -187,6 +97,7 @@ public class ExamScore {
     public void setQzcj(float qzcj) {
         this.qzcj = qzcj;
     }
+
     public float getQzcj() {
         return qzcj;
     }
@@ -194,6 +105,7 @@ public class ExamScore {
     public void setPscj(float pscj) {
         this.pscj = pscj;
     }
+
     public float getPscj() {
         return pscj;
     }
@@ -201,6 +113,7 @@ public class ExamScore {
     public void setKhcj(float khcj) {
         this.khcj = khcj;
     }
+
     public float getKhcj() {
         return khcj;
     }
@@ -208,91 +121,16 @@ public class ExamScore {
     public void setZpcj(float zpcj) {
         this.zpcj = zpcj;
     }
+
     public float getZpcj() {
         return zpcj;
-    }
-
-    public void setKslb(String kslb) {
-        this.kslb = kslb;
-    }
-    public String getKslb() {
-        return kslb;
-    }
-
-    public void setCjlb(String cjlb) {
-        this.cjlb = cjlb;
-    }
-    public String getCjlb() {
-        return cjlb;
-    }
-
-    public void setKssj(int kssj) {
-        this.kssj = kssj;
-    }
-    public int getKssj() {
-        return kssj;
     }
 
     public void setXf(float xf) {
         this.xf = xf;
     }
+
     public float getXf() {
         return xf;
-    }
-
-    public void setXslb(String xslb) {
-        this.xslb = xslb;
-    }
-    public String getXslb() {
-        return xslb;
-    }
-
-    public void setTname1(String tname1) {
-        this.tname1 = tname1;
-    }
-    public String getTname1() {
-        return tname1;
-    }
-
-    public void setStage(int stage) {
-        this.stage = stage;
-    }
-    public int getStage() {
-        return stage;
-    }
-
-    public void setExamt(String examt) {
-        this.examt = examt;
-    }
-    public String getExamt() {
-        return examt;
-    }
-
-    public void setXs(int xs) {
-        this.xs = xs;
-    }
-    public int getXs() {
-        return xs;
-    }
-
-    public void setCjlx(int cjlx) {
-        this.cjlx = cjlx;
-    }
-    public int getCjlx() {
-        return cjlx;
-    }
-
-    public void setChk(int chk) {
-        this.chk = chk;
-    }
-    public int getChk() {
-        return chk;
-    }
-
-    public void setComm(String comm) {
-        this.comm = comm;
-    }
-    public String getComm() {
-        return comm;
     }
 }
--- app/src/main/java/top/yvyan/guettable/Gson/Resit.java
+++ app/src/main/java/top/yvyan/guettable/Gson/Resit.java
@@ -1,34 +1,21 @@
 package top.yvyan.guettable.Gson;
 
-import java.util.Date;
-
 import top.yvyan.guettable.bean.ResitBean;
 
 public class Resit {
     private String name;
-    private String dptno;
-    private String dptname;
-    private String spno;
-    private String spname;
     private String cname;
     private String croomname;
-    private String grade;
     private String bj;
     private String kssj;
-    private String comm;
-    private int xq;
-    private String examt;
     private String term;
     private int bkzt;
     private String courseno;
-    private String stid;
     private String courseid;
     private String examdate;
     private String examtime;
     private String croomno;
     private int seatno;
-    private int zms;
-    private int zrs;
     private int pc;
     private String lb;
     private int ks;
@@ -40,41 +27,15 @@ public class Resit {
     public void setName(String name) {
         this.name = name;
     }
+
     public String getName() {
         return name;
-    }
-
-    public void setDptno(String dptno) {
-        this.dptno = dptno;
-    }
-    public String getDptno() {
-        return dptno;
-    }
-
-    public void setDptname(String dptname) {
-        this.dptname = dptname;
-    }
-    public String getDptname() {
-        return dptname;
-    }
-
-    public void setSpno(String spno) {
-        this.spno = spno;
-    }
-    public String getSpno() {
-        return spno;
-    }
-
-    public void setSpname(String spname) {
-        this.spname = spname;
-    }
-    public String getSpname() {
-        return spname;
     }
 
     public void setCname(String cname) {
         this.cname = cname;
     }
+
     public String getCname() {
         return cname;
     }
@@ -82,20 +43,15 @@ public class Resit {
     public void setCroomname(String croomname) {
         this.croomname = croomname;
     }
+
     public String getCroomname() {
         return croomname;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-    public String getGrade() {
-        return grade;
     }
 
     public void setBj(String bj) {
         this.bj = bj;
     }
+
     public String getBj() {
         return bj;
     }
@@ -103,34 +59,15 @@ public class Resit {
     public void setKssj(String kssj) {
         this.kssj = kssj;
     }
+
     public String getKssj() {
         return kssj;
-    }
-
-    public void setComm(String comm) {
-        this.comm = comm;
-    }
-    public String getComm() {
-        return comm;
-    }
-
-    public void setXq(int xq) {
-        this.xq = xq;
-    }
-    public int getXq() {
-        return xq;
-    }
-
-    public void setExamt(String examt) {
-        this.examt = examt;
-    }
-    public String getExamt() {
-        return examt;
     }
 
     public void setTerm(String term) {
         this.term = term;
     }
+
     public String getTerm() {
         return term;
     }
@@ -138,6 +75,7 @@ public class Resit {
     public void setBkzt(int bkzt) {
         this.bkzt = bkzt;
     }
+
     public int getBkzt() {
         return bkzt;
     }
@@ -145,20 +83,11 @@ public class Resit {
     public void setCourseno(String courseno) {
         this.courseno = courseno;
     }
-    public String getCourseno() {
-        return courseno;
-    }
-
-    public void setStid(String stid) {
-        this.stid = stid;
-    }
-    public String getStid() {
-        return stid;
-    }
 
     public void setCourseid(String courseid) {
         this.courseid = courseid;
     }
+
     public String getCourseid() {
         return courseid;
     }
@@ -166,6 +95,7 @@ public class Resit {
     public void setExamdate(String examdate) {
         this.examdate = examdate;
     }
+
     public String getExamdate() {
         return examdate;
     }
@@ -173,6 +103,7 @@ public class Resit {
     public void setExamtime(String examtime) {
         this.examtime = examtime;
     }
+
     public String getExamtime() {
         return examtime;
     }
@@ -180,6 +111,7 @@ public class Resit {
     public void setCroomno(String croomno) {
         this.croomno = croomno;
     }
+
     public String getCroomno() {
         return croomno;
     }
@@ -187,27 +119,15 @@ public class Resit {
     public void setSeatno(int seatno) {
         this.seatno = seatno;
     }
+
     public int getSeatno() {
         return seatno;
-    }
-
-    public void setZms(int zms) {
-        this.zms = zms;
-    }
-    public int getZms() {
-        return zms;
-    }
-
-    public void setZrs(int zrs) {
-        this.zrs = zrs;
-    }
-    public int getZrs() {
-        return zrs;
     }
 
     public void setPc(int pc) {
         this.pc = pc;
     }
+
     public int getPc() {
         return pc;
     }
@@ -215,6 +135,7 @@ public class Resit {
     public void setLb(String lb) {
         this.lb = lb;
     }
+
     public String getLb() {
         return lb;
     }
@@ -222,6 +143,7 @@ public class Resit {
     public void setKs(int ks) {
         this.ks = ks;
     }
+
     public int getKs() {
         return ks;
     }
