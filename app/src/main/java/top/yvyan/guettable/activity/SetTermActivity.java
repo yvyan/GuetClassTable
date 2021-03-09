07cad8d4c0580a2b81915bd7fe783ef2592f9ac7
Wing <843862803@qq.com>
2021-03-09 21:44:17 +0800

    perf:修改登录为startActivityForResult方式，增强稳定性

--- app/src/main/java/top/yvyan/guettable/activity/SetTermActivity.java
+++ app/src/main/java/top/yvyan/guettable/activity/SetTermActivity.java
@@ -1,6 +1,7 @@
 package top.yvyan.guettable.activity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,7 +14,6 @@ import com.umeng.umcrash.UMCrash;
 import com.xuexiang.xui.widget.button.ButtonView;
 import com.xuexiang.xui.widget.picker.XSeekBar;
 
-import top.yvyan.guettable.MainActivity;
 import top.yvyan.guettable.R;
 import top.yvyan.guettable.data.AccountData;
 import top.yvyan.guettable.data.GeneralData;
@@ -22,6 +22,8 @@ import top.yvyan.guettable.fragment.PersonFragment;
 import top.yvyan.guettable.util.ToastUtil;
 
 public class SetTermActivity extends AppCompatActivity implements View.OnClickListener {
+    public static int REQUEST_CODE = 14;
+    public static int OK = 10;
 
     private TextView stuId;
     private TextView stuName;
@@ -135,16 +137,10 @@ public class SetTermActivity extends AppCompatActivity implements View.OnClickLi
                 //保存星期
                 int week = seekBar.getSelectedNumber() / 10;
                 generalData.setWeek(week);
-
-                GeneralData.newInstance(this).setLastUpdateTime(-1);
-                PersonFragment personFragment = PersonFragment.newInstance();
-                personFragment.updateView();
-                MainActivity mainActivity = (MainActivity) personFragment.getActivity();
-                if (mainActivity != null) {
-                    mainActivity.onClick(0); //切换页面0
-                }
-
+                generalData.setLastUpdateTime(-1);
                 ToastUtil.showToast(getApplicationContext(), "正在导入课表，受教务系统影响，最长需要约30秒，请耐心等待，不要滑动页面");
+                Intent intent = getIntent();
+                setResult(OK, intent);
                 finish();
                 break;
         }
