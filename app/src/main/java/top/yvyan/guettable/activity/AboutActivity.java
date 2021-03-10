41cbc370f0ab2de8b95699367a47ed5cfb6d1ac7
SSbird <594428581@qq.com>
2021-03-10 19:52:56 +0800

    perf:about和setting主题色跟随变更

--- app/src/main/java/top/yvyan/guettable/activity/AboutActivity.java
+++ app/src/main/java/top/yvyan/guettable/activity/AboutActivity.java
@@ -1,15 +1,21 @@
 package top.yvyan.guettable.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
 import top.yvyan.guettable.R;
+import top.yvyan.guettable.data.SingleSettingData;
 import top.yvyan.guettable.util.AppUtil;
+import top.yvyan.guettable.util.BackgroundUtil;
 import top.yvyan.guettable.util.DialogUtil;
 
 import static com.xuexiang.xui.XUI.getContext;
@@ -19,8 +25,14 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SingleSettingData singleSettingData = SingleSettingData.newInstance(this);
+        BackgroundUtil.setPageTheme(this, singleSettingData.getThemeId());
         setContentView(R.layout.activity_about);
-
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        View addStatus = findViewById(R.id.add_status);
+        ViewGroup.LayoutParams lp = addStatus.getLayoutParams();
+        lp.height = lp.height + AppUtil.getStatusBarHeight(Objects.requireNonNull(getApplicationContext()));
+        addStatus.setLayoutParams(lp);
         TextView title = findViewById(R.id.title);
         title.setText(getString(R.string.person_about));
         TextView profileVersion = findViewById(R.id.about_version);
