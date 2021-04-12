f4c1d70508fcec75e3994b1750c22ad9506af810
Wing <843862803@qq.com>
2021-04-12 20:40:59 +0800

    perf:同步在线参数默认值 perf:调整协助测试功能到关于页面

--- app/src/main/java/top/yvyan/guettable/activity/AboutActivity.java
+++ app/src/main/java/top/yvyan/guettable/activity/AboutActivity.java
@@ -1,5 +1,9 @@
 package top.yvyan.guettable.activity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -7,13 +11,19 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.umeng.cconfig.UMRemoteConfig;
+import com.umeng.umcrash.UMCrash;
+
+import java.util.Objects;
 
 import top.yvyan.guettable.R;
+import top.yvyan.guettable.data.SettingData;
 import top.yvyan.guettable.data.SingleSettingData;
+import top.yvyan.guettable.data.TokenData;
 import top.yvyan.guettable.service.table.CommFunc;
 import top.yvyan.guettable.util.AppUtil;
 import top.yvyan.guettable.util.BackgroundUtil;
 import top.yvyan.guettable.util.DialogUtil;
+import top.yvyan.guettable.util.ToastUtil;
 
 import static com.xuexiang.xui.XUI.getContext;
 
@@ -62,5 +72,47 @@ public class AboutActivity extends AppCompatActivity {
 
     public void statement(View view) {
         DialogUtil.showTextDialog(this, getResources().getString(R.string.first_about));
+    }
+
+    public void helpTest(View view) {
+        AppUtil.reportFunc(getContext(), getResources().getString(R.string.person_help_test));
+        helpTest();
+    }
+
+    /**
+     * 协助测试
+     */
+    public void helpTest() {
+        if (AppUtil.isWifi(Objects.requireNonNull(getContext()))) {
+            DialogUtil.showTextDialog(this, "为了保证测试顺利，请关闭WIFI，连接数据网络后进行测试。");
+        } else {
+            if (SettingData.newInstance(getContext()).isDevelopMode()) {
+                Intent intent = new Intent(getContext(), HelpTestActivity.class);
+                startActivity(intent);
+            } else {
+                ToastUtil.showToast(this, "请不要退出页面或者切换网络，正在获取凭证，最多需要30s，请稍后！");
+                new Thread(() -> {
+                    TokenData tokenData = TokenData.newInstance(getContext());
+                    int n = tokenData.refresh();
+                    runOnUiThread(() -> {
+                        try {
+                            if (n == 0) {
+                                //获取剪贴板管理器：
+                                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                                // 创建普通字符型ClipData
+                                ClipData mClipData = ClipData.newPlainText("Label", tokenData.getCookie());
+                                // 将ClipData内容放到系统剪贴板里。
+                                cm.setPrimaryClip(mClipData);
+                                DialogUtil.showTextDialog(this, "感谢协助，凭证复制成功，您现在可以发送给开发者了！");
+                            } else {
+                                DialogUtil.showTextDialog(this, "获取失败，请稍后重试。");
+                            }
+                        } catch (Exception e) {
+                            UMCrash.generateCustomLog(e, "helpTest");
+                        }
+                    });
+                }).start();
+            }
+        }
     }
 }
