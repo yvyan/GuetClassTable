7eef5da261fbc9b9b2574825ad706424e7ebca40
Wing <843862803@qq.com>
2021-05-28 19:59:28 +0800

    perf:调整包结构 perf:捕获异常，降低闪退概率

--- app/src/main/java/top/yvyan/guettable/service/MoreFunService.java
+++ app/src/main/java/top/yvyan/guettable/service/MoreFunService.java
@@ -23,53 +23,51 @@ public class MoreFunService {
 
     public void update() {
         new Thread(() -> {
-            if (accountData.getIsLogin()) {
-                setView(91); //显示：尝试同步
-                int state = -2;
-                try {
+            try {
+                if (accountData.getIsLogin()) {
+                    setView(91); //显示：尝试同步
+                    int state;
                     state = iMoreFun.updateData(tokenData.getCookie());
-                } catch (Exception e) {
-                    UMCrash.generateCustomLog(e, "update");
-                }
-                if (state == 5 || state == -2) { //同步成功或网络错误
-                    setView(state);
-                    return;
-                }
-                setView(92); //显示：正在登录
-                state = tokenData.refresh();
-                if (state == -2) {
+                    if (state == 5 || state == -2) { //同步成功或网络错误
+                        setView(state);
+                        return;
+                    }
+                    setView(92); //显示：正在登录
                     state = tokenData.refresh();
-                }
-                if (state != 0) {
+                    if (state == -2) {
+                        state = tokenData.refresh();
+                    }
+                    if (state != 0) {
+                        setView(state);
+                        return;
+                    }
+                    setView(93); //显示：正在同步
+                    state = iMoreFun.updateData(tokenData.getCookie());
                     setView(state);
-                    return;
+                } else {
+                    setView(2);
                 }
-                setView(93); //显示：正在同步
-                state = iMoreFun.updateData(tokenData.getCookie());
-                setView(state);
-
-            } else {
-                setView(2);
+            } catch (Exception e) {
+                UMCrash.generateCustomLog(e, "MoreFunService");
             }
         }).start();
     }
 
     /**
      * state记录当前状态
-     *  0 : 登录成功
-     *  1 : 登录失效
-     *  2 : 未登录
-     *
-     *  5 : 通用获取数据成功
-     *
+     * 0 : 登录成功
+     * 1 : 登录失效
+     * 2 : 未登录
+     * <p>
+     * 5 : 通用获取数据成功
+     * <p>
      * -1 : 密码错误
      * -2 : 网络错误/未知错误
      * -3 : 验证码连续错误
-     *
+     * <p>
      * 91 : 尝试同步
      * 92 : 正在登录
      * 93 : 正在同步
-     *
      */
     private void setView(int state) {
         String hint;
