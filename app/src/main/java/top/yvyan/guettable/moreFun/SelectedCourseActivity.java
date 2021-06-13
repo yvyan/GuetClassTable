65af85aa5301d740946796f4c1bf851162c8da3e
SSbird <594428581@qq.com>
2021-06-13 15:29:30 +0800

    perf: 已选课程添加查询结果的Toast提示 fix: 已选课程查询期间网络切换可能闪退的问题

--- app/src/main/java/top/yvyan/guettable/moreFun/SelectedCourseActivity.java
+++ app/src/main/java/top/yvyan/guettable/moreFun/SelectedCourseActivity.java
@@ -4,6 +4,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.xuexiang.xui.widget.spinner.materialspinner.MaterialSpinner;
+import com.xuexiang.xui.widget.toast.XToast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -109,7 +110,10 @@ public class SelectedCourseActivity extends BaseFuncActivity implements IMoreFun
                     adapter = new SelectedCourseAdapter(selectedCourse);
                     rv.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
+                    XToast.success(this, "查询成功").show();
                 });
+            } else {
+                runOnUiThread(() -> XToast.error(this, "请求出错").show());
             }
         }).start();
     }
