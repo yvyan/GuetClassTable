07e93888e5a4ab28de4e6a04b4a014d797685343
Wing <843862803@qq.com>
2021-06-18 00:03:31 +0800

    fix:密码大于16位时vpn登录失败的问题

--- app/src/main/java/top/yvyan/guettable/util/RSAUtil.java
+++ app/src/main/java/top/yvyan/guettable/util/RSAUtil.java
@@ -1,12 +1,13 @@
 package top.yvyan.guettable.util;
 
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.PublicKey;
 import java.security.spec.RSAPublicKeySpec;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class RSAUtil {
     private static final String publicExponentStr = "010001";
@@ -16,7 +17,7 @@ public class RSAUtil {
      * CAS登录RSA公钥加密
      *
      * @param text 未加密字符串
-     * @return     RSA加密后的字符串(16进制字符串)
+     * @return RSA加密后的字符串(16进制字符串)
      */
     public static String CASEncryption(String text) {
         try {
@@ -33,42 +34,6 @@ public class RSAUtil {
         } catch (Exception ignored) {
         }
         return null;
-    }
-
-    /**
-     * VPN登录RSA加密
-     *
-     * @param text 未加密字符串
-     * @return     RSA加密后的字符串(16进制字符串)
-     */
-    public static String VPNEncryption(String text) {
-        int[] key = {134, 8, 187, 0, 251, 59, 238, 74, 176, 180, 24, 67, 227, 252, 205, 80};
-        int pwd_len = text.length();
-        try {
-            if (text.length() % 16 != 0) {
-                int need_num = 16 - text.length() % 16;
-                StringBuilder pwd_builder = new StringBuilder();
-                pwd_builder.append(text);
-                for (int i = 0; i < need_num; i++) {
-                    pwd_builder.append("0");
-                }
-                text = pwd_builder.toString();
-            }
-            byte[] pwd_bytes = text.getBytes(StandardCharsets.UTF_8);
-            for (int i = 0; i < pwd_bytes.length; i++) {
-                pwd_bytes[i] ^= key[i % 16];
-            }
-            StringBuilder encrypt_builder = new StringBuilder();
-            encrypt_builder.append("77726476706e6973617765736f6d6521");
-            for (int i = 0; i < pwd_len; i++) {
-                byte b = pwd_bytes[i];
-                encrypt_builder.append(String.format("%02x", b));
-            }
-            return encrypt_builder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
     /**
@@ -93,5 +58,38 @@ public class RSAUtil {
             }
         }
         return hs.toString();
+    }
+
+    public static String VPNEncryption(String text) {
+        String key = "wrdvpnisawesome!";
+        String iv = "wrdvpnisawesome!";
+        byte[] result = encrypt_AES(text, key, iv);
+        if (result != null) {
+            StringBuilder encrypt_builder = new StringBuilder();
+            encrypt_builder.append("77726476706e6973617765736f6d6521");
+            for (byte b : result) {
+                encrypt_builder.append(String.format("%02x", b));
+            }
+            return encrypt_builder.toString();
+        }
+        return null;
+    }
+
+    /**
+     * @param content 待加密内容
+     * @param key     密钥
+     * @return byte[]加密结果用byte数组表示
+     */
+    public static byte[] encrypt_AES(String content, String key, String iv) {
+        try {
+            Cipher aesECB = Cipher.getInstance("AES/CFB/NoPadding");
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(iv.getBytes());
+            aesECB.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+            return aesECB.doFinal(content.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
